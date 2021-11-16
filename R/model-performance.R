7db8af5983114273ce5503f52415d3dc95e3121a
mraskj <mathias.rask@hotmail.com>
2021-11-16 18:27:13 +0100

    Update R script

--- R/model-performance.R
+++ R/model-performance.R
@@ -1,14 +1,14 @@
 library(ggplot2)
 library(data.table)
 
-setwd('/home/rask/Dropbox/Facing-Politics/')
+setwd('/home/rask/Dropbox/PreReg-FacingElection/')
 
-df_women <- read.csv('model-performance_women.csv', sep=',', header=T)
+df_women <- read.csv('model/model-performance_women.csv', sep=',', header=T)
 df_women['X'] <- NULL
 setDT(df_women)
 df_women[, gender := 'women']
 
-df_men <- read.csv('model-performance_men.csv', sep=',', header=T)
+df_men <- read.csv('model/model-performance_men.csv', sep=',', header=T)
 df_men['X'] <- NULL
 setDT(df_men)
 df_men[, gender := 'men']
@@ -21,7 +21,7 @@ median_val_bal_acc <- df[,median(val_bal_acc) ,by=gender]
 # make plot
 p <- ggplot(df, aes(x=val_bal_acc, fill=gender)) +
   geom_density(alpha=.5, color='grey30') + 
-  geom_vline(data=mean_val_bal_acc, aes(xintercept=V1, color=gender),
+  geom_vline(data=median_val_bal_acc, aes(xintercept=V1, color=gender),
              linetype="dashed", show.legend = F) + 
   labs(x='Validation balanced accuracy', title = 'Distribution of model performances') + 
   theme_light() + 
@@ -32,7 +32,8 @@ p <- ggplot(df, aes(x=val_bal_acc, fill=gender)) +
         axis.text.y=element_blank(),
         axis.ticks.y=element_blank(),
         plot.title = element_text(hjust = 0.5))
-ggsave('model_performance.jpg', p)
+ggsave('plots/model_performance.jpg', p)
+
 
 
 ## VALIDATION WOMEN
@@ -51,5 +52,30 @@ df_women[val_bal_acc==val]
 
 # average
 mean(df_women$val_bal_acc)
+mean(df_women$val_loss)
+mean(df_women$train_bal_acc)
+mean(df_women$train_loss)
+
+## VALIDATION MEN
+
+# MAIN model
+val <- sort(df_men$val_bal_acc)[10]
+df_men[val_bal_acc==val]
+
+# worst model
+val <- sort(df_men$val_bal_acc)[1]
+df_men[val_bal_acc==val]
+
+# best model
+val <- sort(df_men$val_bal_acc)[nrow(df_men)]
+df_men[val_bal_acc==val]
+
+# average
+mean(df_men$val_bal_acc)
+mean(df_men$val_loss)
+mean(df_men$train_bal_acc)
+mean(df_men$train_loss)
+
+
 
 
